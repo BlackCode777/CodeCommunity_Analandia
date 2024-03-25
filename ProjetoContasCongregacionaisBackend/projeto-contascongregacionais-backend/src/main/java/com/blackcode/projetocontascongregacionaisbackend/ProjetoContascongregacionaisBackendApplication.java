52b94baab0b9ed7075044ea74554d92ddfd60d54
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-25 19:28:46 -0300

    [ ContasCongregacionais ] - Iniciando o projeto Spring boot com MongoDB

---
+++ ProjetoContasCongregacionaisBackend/projeto-contascongregacionais-backend/src/main/java/com/blackcode/projetocontascongregacionaisbackend/ProjetoContascongregacionaisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.projetocontascongregacionaisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoContascongregacionaisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoContascongregacionaisBackendApplication.class, args);
+	}
+
+}
---
+++ ProjetoContasCongregacionaisBackend/projeto-contascongregacionais-backend/src/test/java/com/blackcode/projetocontascongregacionaisbackend/ProjetoContascongregacionaisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcode.projetocontascongregacionaisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoContascongregacionaisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

21b7f6870c361864a770cdab076bfd69c9128487
Anderson Martins <BlackCode77.1980@gmail.com>
2024-05-03 18:23:23 -0300

    ...

--- ProjetoContasCongregacionaisBackend/projeto-contascongregacionais-backend/src/main/java/com/blackcode/projetocontascongregacionaisbackend/api/RecibosDashboardAPI.java
+++ ProjetoContasCongregacionaisBackend/projeto-contascongregacionais-backend/src/main/java/com/blackcode/projetocontascongregacionaisbackend/api/RecibosDashboardAPI.java
@@ -1,14 +1,10 @@
 package com.blackcode.projetocontascongregacionaisbackend.api;
 
-import static java.util.Objects.*;
-
 import java.util.List;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,36 +24,39 @@ public class RecibosDashboardAPI {
     ReciboDashboardService reciboService;
     private static Logger LOG = LoggerFactory.getLogger(RecibosDashboardAPI.class);
 
-    @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/getRecibosDashboardFindAll")
-    public ResponseEntity<Object> getRecibosDashboardFindAll(@RequestParam(required = false) String param) {
-        JSONObject mensagemJson = new JSONObject();
-        try {
-            List<ReciboDashboardClasse> recibo = reciboService.findAll();
-
-            System.out.println("Recibo _############_ : " + recibo);
-
-            if (recibo.isEmpty()) {
-                mensagemJson.put("message", "Nenhum recibo encontrado.");
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(mensagemJson.toString());
-            } else {
-                return ResponseEntity.ok(recibo.get(0));
-            }
-        } catch (Exception e) {
-            LOG.error("Erro ao buscar recibos do dashboard", e);
-            String mensagem = e.getMessage() + (isNull(e.getCause()) ? "" : " Causa: " + e.getCause().getMessage());
-            mensagemJson.put("erro", mensagem);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(mensagemJson.toString());
-        }
-    }
-
+    // @CrossOrigin(origins = "http://localhost:4200")
     // @GetMapping("/getRecibosDashboardFindAll")
-    // public ResponseEntity<ReciboDashboardClasse> getRecibosDashboardFindAll(
-    // @RequestParam(required = false) String param) {
+    // public ResponseEntity<Object>
+    // getRecibosDashboardFindAll(@RequestParam(required = false) String param) {
+    // ObjectMapper mensagemJson = new ObjectMapper();
+    // try {
     // List<ReciboDashboardClasse> recibo = reciboService.findAll();
-    // return recibo.isEmpty() ? ResponseEntity.noContent().build() :
-    // ResponseEntity.ok(recibo.get(0));
+
+    // System.out.println("Recibo _############_ : " + recibo);
+
+    // if (recibo.isEmpty()) {
+    // mensagemJson.put("message", "Nenhum recibo encontrado.");
+    // return
+    // ResponseEntity.status(HttpStatus.NO_CONTENT).body(mensagemJson.toString());
+    // } else {
+    // return ResponseEntity.ok(recibo.get(0));
     // }
+    // } catch (Exception e) {
+    // LOG.error("Erro ao buscar recibos do dashboard", e);
+    // String mensagem = e.getMessage() + (isNull(e.getCause()) ? "" : " Causa: " +
+    // e.getCause().getMessage());
+    // mensagemJson.put("erro", mensagem);
+    // return
+    // ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(mensagemJson.toString());
+    // }
+    // }
+
+    @GetMapping("/getRecibosDashboardFindAll")
+    public ResponseEntity<ReciboDashboardClasse> getRecibosDashboardFindAll(
+            @RequestParam(required = false) String param) {
+        List<ReciboDashboardClasse> recibo = reciboService.findAll();
+        return recibo.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(recibo.get(0));
+    }
 
 }
 
